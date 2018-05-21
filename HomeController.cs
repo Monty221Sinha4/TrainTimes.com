ba888c3ffe6cbed7e71b1bc3c3bdd0944cbd3ef9
Monty Sinha <monty@vayaconnect.com>
2018-05-22 00:14:59 +0100

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Traintime.com.Controllers.Home
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //GET: About US 
+        public ActionResult AboutUS()
+        {
+            return View();
+        }
+        //GET :Contact
+      public ActionResult Contact()
+        {
+            return View();
+        }
+        //GET : Departure table 
+        public ActionResult Departure()
+        {
+            return View();
+        }
+        // GET :Arrivals table
+        public ActionResult Arrival()
+        {
+            return View();
+        }
+    }
+}
